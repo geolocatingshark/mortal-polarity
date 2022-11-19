03bcb859498efb0e595a343480ef603d913a8b42
geolocatingshark <geolocatingshark@gmail.com>
2022-11-20 02:09:23 +0530

    Follow channels where possible
    This commit follows channels and skips posting to them manually
    Channels are configured in cfg

--- polarity/xur.py
+++ polarity/xur.py
@@ -24,6 +24,7 @@ class XurPostSettings(UrlPostSettings, Base):
 
 class XurAutopostChannel(UrlAutopostChannel, Base):
     settings_records = XurPostSettings
+    follow_channel = cfg.xur_follow_channel_id
 
 
 class XurSignal(BaseUrlSignal):
